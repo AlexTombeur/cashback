101fad7211ff01dc1f2f6921677731e3d48abc42
Alexandre Tombeur <alexandre.tombeur@gmail.com>
2016-08-23 17:26:31 +0200

    expense and message controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,31 @@
 Rails.application.routes.draw do
+  # get 'messages/index'
+
+  # get 'messages/new'
+
+  # get 'messages/create'
+
+  # get 'expenses/index'
+
+  # get 'expenses/new'
+
+  # get 'expenses/show'
+
+  # get 'expenses/create'
+
+  # get 'expenses/edit'
+
+  # get 'expenses/update'
+
+  # get 'expenses/destroy'
+
   root to: 'pages#home'
   devise_for :users
 
   resource :profile, only: [:show, :edit, :update]
 
   resources :expenses do
-    resources :messages, only: [:index, :create]
+    resources :messages, only: [:index, :new, :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
