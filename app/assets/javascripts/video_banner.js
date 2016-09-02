f081f5a95eac491cfac209d3f68c12e7f27aa374
Alexandre Tombeur <alexandre.tombeur@gmail.com>
2016-09-02 17:11:56 +0200

    fix for iPhone6

--- app/assets/javascripts/video_banner.js
+++ app/assets/javascripts/video_banner.js
@@ -19,7 +19,6 @@ function scaleVideoContainer() {
 
     var height = $(window).height() + 5;
     var unitHeight = parseInt(height) + 'px';
-    $('.homepage-hero-module').css('height',unitHeight);
 
 }
 
